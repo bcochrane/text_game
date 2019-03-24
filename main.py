02ff1f6ffb4cbc505875ecc917442563a1c69eb9
Brian Cochrane <brian.j.cochrane@gmail.com>
2019-03-24 00:01:56 -0700

    basic room generation

--- main.py
+++ main.py
@@ -6,16 +6,32 @@ class Map:
         self.x_size = x_size
         self.y_size = y_size
 
+        self.map_start_coords = (int(self.x_size / 2), int(self.y_size / 2))
+        self.rooms = {}
+
+    def generate_map(self):
+        # create a room
+        # randomly create exits; disallow creation of exit if adjacent room exists and does't have a matching exit
+        # create room adjacent to exits if one does not already exist
+
+        # room.print_room()
+
+        self.rooms[self.map_start_coords] = Room(desc='first room')
+        # self.rooms[(5, 5)].print_room()
+
+
+
     def print_map(self):
         pass
 
 
 class Room:
-    def __init__(self, x_coord: int, y_coord: int, exits: list, description: str = ''):
-        self.x_coord = x_coord
-        self.y_coord = y_coord
+    def __init__(self, exits: list = [], desc: str = ''):
         self.exits = exits
-        self.description = description
+        self.desc = desc
+
+        if not self.exits:
+            self.exits = random.sample(('N', 'E', 'S', 'W'), k=random.randrange(1, 5))
 
     def print_room(self):
         n = '  ' if 'N' in self.exits else '--'
@@ -28,19 +44,11 @@ class Room:
 
 
 def main():
-    generate_map()
+    map1 = Map()
+    map1.generate_map()
+
     # display map
     pass
-
-
-def generate_map():
-    # create a room
-    # randomly create exits; disallow creation of exit if adjacent room exists and does't have a matching exit
-    # create room adjacent to exits if one does not already exist
-
-    exits = random.sample(('N', 'E', 'S', 'W'), k=random.randrange(1, 5))
-    room = Room(0, 0, exits)
-    room.print_room()
 
 
 if __name__ == '__main__':
