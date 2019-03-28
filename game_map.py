4786bbd8b80706eaff03d6c510eb1ea2eba6fbbb
Brian Cochrane <brian.j.cochrane@gmail.com>
2019-03-27 23:14:23 -0700

    Converted Map class several methods to module functions. Adjustments for PEP 8 compliance. Added some comments. Started remapping the logic for Map.generate_map()

--- game_map.py
+++ game_map.py
@@ -1,83 +1,107 @@
 import random
 
 
+def sum_coordinates(coordinates1: tuple, coordinates2: tuple):
+    x = coordinates1[0] + coordinates2[0]
+    y = coordinates1[1] + coordinates2[1]
+    return (x, y)
+
+
+def generate_random_doors(min_doors: int = 1, max_doors: int = 4):
+    return random.sample(
+        ('N', 'E', 'S', 'W'),
+        k=random.randrange(min_doors, max_doors + 1))
+
+
+def get_adjacent_room_coordinates(
+        current_room_coordinates: tuple, door_direction: str):
+    if door_direction == 'N':
+        return sum_coordinates(current_room_coordinates, (0, -1))
+    elif door_direction == 'E':
+        return sum_coordinates(current_room_coordinates, (1, 0))
+    elif door_direction == 'S':
+        return sum_coordinates(current_room_coordinates, (0, 1))
+    elif door_direction == 'W':
+        return sum_coordinates(current_room_coordinates, (-1, 0))
+    else:
+        return None
+
+
+def get_opposing_door(door_direction):
+    if door_direction == 'N':
+        return 'S'
+    elif door_direction == 'E':
+        return 'W'
+    elif door_direction == 'S':
+        return 'N'
+    elif door_direction == 'W':
+        return 'E'
+    else:
+        return None
+
+
 class Map:
     def __init__(self, x_size: int = 10, y_size: int = 10):
         self.x_size = x_size
         self.y_size = y_size
 
-        self.map_start_coords = (int(self.x_size / 2), int(self.y_size / 2))
+        # find the center of the map
+        self.map_start_coordinates = (
+            int(self.x_size / 2), int(self.y_size / 2))
+
+        # Map data is stored in a dictionary. The keys are 2-element
+        # tuples representing (x, y) coordinates, and the values are
+        # Room objects.
         self.rooms = {}
 
     def generate_map(self):
-        # create a room
-        # randomly create exits; disallow creation of exit if adjacent room exists and does't have a matching exit
-        # create room adjacent to exits if one does not already exist
+        # 1. Create a room.
+        # 2. Create random doors in the current room.
+        # 3. Check for rooms adjacent to the current room.
+        # 4. If adjacent rooms don't have matching doors, remove the
+        #    corresponding doors from the current room.
+        # 5. If the adjacent rooms have doors facing the current room,
+        #    and the current room doesn't have matching doors, add
+        #    matching doors to the current room.
+        # 6. Check for rooms adjacent to current room's doors. If they
+        #    don't exist, add coordinates to stack.
 
-        current_room_coords = self.map_start_coords
-        exits = self.get_random_exits(min_exits=1, max_exits=3)
-        self.rooms[current_room_coords] = Room(exits)
-        current_room = self.rooms[current_room_coords]
-        for exit in current_room.exits:
-            adjacent_room_coords = self.get_adjacent_room_coords(current_room_coords, exit)
-            if self.check_if_room_exists(adjacent_room_coords):
-                adjacent_room = self.rooms[adjacent_room_coords]
+        current_room_coordinates = self.map_start_coordinates
+        doors = generate_random_doors(min_doors=1, max_doors=3)
+        self.rooms[current_room_coordinates] = Room(doors)
+        current_room = self.rooms[current_room_coordinates]
+        for door in current_room.doors:
+            adjacent_room_coordinates = get_adjacent_room_coordinates(
+                current_room_coordinates, door)
+            if self.check_if_room_exists(adjacent_room_coordinates):
+                adjacent_room = self.rooms[adjacent_room_coordinates]
             else:
-                self.rooms[adjacent_room_coords] = Room(exits=self.get_random_exits(min_exits=2, max_exits=3))
-                adjacent_room = self.rooms[adjacent_room_coords]
-                opposing_exit = self.get_opposing_exit(exit)
-                if not opposing_exit in adjacent_room.exits:
-                    adjacent_room.create_exit(opposing_exit)
+                self.rooms[adjacent_room_coordinates] = Room(
+                    doors=generate_random_doors(min_doors=2, max_doors=3))
+                adjacent_room = self.rooms[adjacent_room_coordinates]
+                opposing_door = get_opposing_door(door)
+                if opposing_door not in adjacent_room.doors:
+                    adjacent_room.create_door(opposing_door)
 
-    def check_if_room_exists(self, coords: tuple):
+    def check_if_room_exists(self, coordinates: tuple):
         try:
-            if self.rooms[coords]:
+            if self.rooms[coordinates]:
                 return True
-        except:
+        except KeyError:
             return False
-
-    def sum_coords(self, coords1: tuple, coords2: tuple):
-        return (coords1[0] + coords2[0], coords1[1] + coords2[1])
-
-    def get_random_exits(self, min_exits: int=1, max_exits: int = 4):
-        return random.sample(('N', 'E', 'S', 'W'), k=random.randrange(min_exits, max_exits + 1))
-
-    def get_adjacent_room_coords(self, current_room_coords: tuple, exit_direction: str):
-        if exit_direction == 'N':
-            return self.sum_coords(current_room_coords, (0, -1))
-        elif exit_direction == 'E':
-            return self.sum_coords(current_room_coords, (1, 0))
-        elif exit_direction == 'S':
-            return self.sum_coords(current_room_coords, (0, 1))
-        elif exit_direction == 'W':
-            return self.sum_coords(current_room_coords, (-1, 0))
-        else:
-            return None
-
-    def get_opposing_exit(self, exit_direction):
-        if exit_direction == 'N':
-            return 'S'
-        elif exit_direction == 'E':
-            return 'W'
-        elif exit_direction == 'S':
-            return 'N'
-        elif exit_direction == 'W':
-            return 'E'
-        else:
-            return None
 
     def print_map(self, x_min: int, y_min: int, x_max: int, y_max: int):
         map_text_rows = []
         for y in range(y_min, y_max + 1):
             map_text_row = ['', '', '', '', '']
             for x in range(x_min, x_max + 1):
-                current_coords = (x, y)
-                if self.check_if_room_exists(current_coords):
-                    current_room = self.rooms[current_coords]
-                    n = '  ' if 'N' in current_room.exits else '--'
-                    e = ' ' if 'E' in current_room.exits else '|'
-                    s = '  ' if 'S' in current_room.exits else '--'
-                    w = ' ' if 'W' in current_room.exits else '|'
+                current_coordinates = (x, y)
+                if self.check_if_room_exists(current_coordinates):
+                    current_room = self.rooms[current_coordinates]
+                    n = '  ' if 'N' in current_room.doors else '--'
+                    e = ' ' if 'E' in current_room.doors else '|'
+                    s = '  ' if 'S' in current_room.doors else '--'
+                    w = ' ' if 'W' in current_room.doors else '|'
                     room_text = [
                         f'+--{n}--+',
                         f'|      |',
@@ -103,14 +127,14 @@ class Map:
 
 
 class Room:
-    def __init__(self, exits: list, desc: str = ''):
-        self.exits = exits
+    def __init__(self, doors: list, desc: str = ''):
+        self.doors = doors
         self.desc = desc
 
-    def create_exit(self, exit: str):
-        if exit not in self.exits:
-            self.exits.append(exit)
+    def create_door(self, door: str):
+        if door not in self.doors:
+            self.doors.append(door)
 
-    def delete_exit(self, exit):
-        if exit in self.exits:
-            self.exits.remove(exit)
+    def delete_door(self, door):
+        if door in self.doors:
+            self.doors.remove(door)
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def main():
     map1 = Map()
     map1.generate_map()
 
-    x_min = map1.map_start_coords[0] - 2
-    y_min = map1.map_start_coords[1] - 2
-    x_max = map1.map_start_coords[0] + 2
-    y_max = map1.map_start_coords[1] + 2
+    x_min = map1.map_start_coordinates[0] - 2
+    y_min = map1.map_start_coordinates[1] - 2
+    x_max = map1.map_start_coordinates[0] + 2
+    y_max = map1.map_start_coordinates[1] + 2
     map1.print_map(x_min, y_min, x_max, y_max)
 
 
